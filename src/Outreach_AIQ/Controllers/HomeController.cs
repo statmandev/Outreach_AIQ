83af40b5af572f076a1d5da3ee2aa086c2b03846
statmandev <anon@anon.com>
2017-01-27 11:57:21 -0600

    Added Post Functionality
    Created a model class to store vehicle information. Created HttpPost
    method on the controller to accept user input and calculate annual
    premium. Modified the index view page to accept the updated view with
    the calculated annual premium stored to the model and displayed.

--- src/Outreach_AIQ/Controllers/HomeController.cs
+++ src/Outreach_AIQ/Controllers/HomeController.cs
@@ -1,18 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http; // to load FormCollection class for handling form data
+using Outreach_AIQ.Models; // models folder containing custom Quote class
 
 namespace Outreach_AIQ.Controllers
 {
-    public class HomeController : Controller
+	public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+		// Get Method
+		public IActionResult Index()
+		{
+			// initialize Quote model object when default page loads
+			Quote quote = new Quote
+			{
+				Age = 0,
+				ZipCode = 0,
+				Mileage = 0,
+				Year = 0,
+				Make = "Not Selected",
+				Model = "Not Selected",
+				Premium = 0
+			};
 
+			return View(quote);
+		}
+
+		// Post Method
+		[HttpPost]
+		public IActionResult Index(FormCollection collection, Quote model) // collection will contain all form information from Index View Post
+        {
+			// gather form data
+			int temp = 0; // define int to hold age since we cannot use a reference of model.Age during string conversion
+
+			int.TryParse(Request.Form["Age"], out temp); // TryParse to convert from string to int and assign to temp
+			model.Age = temp; // assign converted int to Age member variable of our Quote model
+
+			int.TryParse(Request.Form["ZipCode"], out temp);
+			model.ZipCode = temp;
+
+			int.TryParse(Request.Form["Mileage"], out temp);
+			model.Mileage = temp;
+
+			int.TryParse(Request.Form["Year"], out temp);
+			model.Year = temp;
+
+			model.Make = Request.Form["Make"];
+			model.Model = Request.Form["Model"];
+
+			// calculate annual premium
+			model.Premium = 100;
+            return View(model);
+        }
+		
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
---
+++ src/Outreach_AIQ/Models/Quote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Outreach_AIQ.Models
+{
+    public class Quote
+    {
+		public int Age { get; set; }
+		public int ZipCode { get; set; }
+		public int Mileage { get; set; }
+		public int Year { get; set; }
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Premium { get; set; }
+	}
+}
